5f8e0cb56b67e49b6280d4dda0175c85999a0188
Andrew Budziszek <andrew@budziszek.com>
2022-03-09 11:02:06 -0600

    added buymeacoffee link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import QuestionMarkCircleIcon from '@heroicons/react/outline/QuestionMarkCircleIcon';
 import CogIcon from '@heroicons/react/solid/CogIcon';
+import HeartIcon from '@heroicons/react/solid/HeartIcon';
 
 export default function Header() {
     const buttonClassName = 'hover:text-gray-700'
@@ -9,8 +9,8 @@ export default function Header() {
         <div className="place-items-center text-white">
             <div className="grid grid-cols-4 inline-flex items-center">
                 <div className='header-left align-center flex justify-start'>
-                    <button className={buttonClassName}>
-                        <QuestionMarkCircleIcon className={iconClassName} />
+                    <button className={buttonClassName} onClick={() => {window.open('https://www.buymeacoffee.com/SonBrooks', '_blank')}}>
+                        <HeartIcon className={iconClassName} />
                     </button>
                 </div>
                 <div className='text-4xl font-bold col-span-2 title align-center py-2'>
