68c26a2e649903e4d2af26160c29af4e5d77235b
Andrew <andrew@proton.lan>
2023-10-05 10:14:04 -0500

    Adding phrases... A thought. Add this to the page in a random fashion?

---
+++ src/assets/phrases.ts
@@ -0,0 +1,14 @@
+const phrases = [
+  'Hello, Whurld!',
+  'It\'s quite the day!',
+  'Sieze the means of production!',
+  'Happy to see ya!',
+  'Howdy, partner!',
+  'Down with the bourgeoisie!',
+  'I\'m a little teapot!',
+  'The world is your oyster!',
+  'Small thoughts, big dreams!',
+  'AI is taking over!',
+  'Can confirm, aliens are real!',
+  'Time to get schwifty!',
+]
